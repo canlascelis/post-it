af86e0c0bd9a8993d870d030b780018c7fa560ff
trjeremiahcelis <59946355+canlascelis@users.noreply.github.com>
2022-10-01 17:07:07 +0800

    added logo

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
 nav.navbar.navbar-expand-lg.navbar-light.bg-light(:class="isDarkMode ? 'navbar-dark bg-dark' : ''")
     .container
-        RouterLink.navbar-brand(to="/") 
+        router-link.navbar-brand(to="/") 
             img.logo(:src="require('@/assets/post-it-logo.svg')", alt="post-it-logo")
         button.navbar-toggler(type="button" data-toggle="collapse" data-target="#navbarContents")
             span.navbar-toggler-icon
         .collapse.navbar-collapse#navbarContents
             ul.navbar-nav.mr-auto.mt-2.mt-lg-0
                 li.navbar-item
-                    RouterLink.nav-link(to="/") Home
+                    router-link.nav-link(to="/") Home
                 li.navbar-item 
-                    RouterLink.nav-link(to="/about") About 
+                    router-link.nav-link(to="/about") About 
             form.form-inline.my-2.my-lg-0
                 .input-group 
                     input.form-control.mr-sm-2(type="search" placeholder="Search")
-                    RouterLink.btn.btn-primary(to="/Login") Login
+                    router-link.btn.btn-primary(to="/Login") Login
                     button.btn.btn-outline(@click="darkMode()" type="button")
                         font-awesome-icon(icon="fa-solid fa-moon" :color="isDarkMode ? 'white' : 'black'" )
 </template>
